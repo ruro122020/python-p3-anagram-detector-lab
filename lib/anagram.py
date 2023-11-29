469dcf4bf7eaedb87fb7efb4c4a50f0e4b08aa3b
Ruth <ruro122020@gmail.com>
2023-11-29 16:31:54 -0500

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        words = list()
+        for word in word_list:
+            letters = [letter for letter in word]
+            if sorted(self.word) == sorted(letters):
+                words.append(word)
+        if len(words) > 0:
+            return words
+        else: 
+            return words
